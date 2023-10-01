bddfec3c4c55961404ef48618cf2df7fbdef38fc
KISHAN KUMAR YADAV <138916451+Philomathic-boy@users.noreply.github.com>
2023-10-01 23:44:01 +0530

    #HACTOBERFEST UDATE CODE
    Function and variable names are written in lowercase with underscores (snake_case) following Python's naming conventions.
    
    Added a docstring or comment to the bubble_sort function to explain what it does.
    
    Removed unnecessary comments that were only explaining the loop ranges. The code is self-explanatory without these comments.
    
    Changed the loop that prints the sorted array to use a for element in arr loop for cleaner and more Pythonic code.

--- Programs/bubble_sort.py
+++ Programs/bubble_sort.py
@@ -1,26 +1,20 @@
-# Python program for implementation of Bubble Sort 
-  
-def bubbleSort(arr): 
-    n = len(arr) 
-  
-    # Traverse through all array elements 
-    for i in range(n-1): 
-    # range(n) also work but more than needed. 
-  
-        # Last i elements are already in place 
-        for j in range(0, n-i-1): 
-  
-            # traverse from 0 to n-i-1 
-            # Swap found is greater 
-            # than the next element 
-            if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
-  
-# Driver code to test above 
-arr = [64, 34, 25, 12, 2, 1, 90] 
-  
-bubbleSort(arr) 
-  
-print ("Sorted array is:") 
-for i in range(len(arr)): 
-    print ("%d" %arr[i]),
+def bubble_sort(arr):
+    n = len(arr)
+    
+    # Traverse through all array elements
+    for i in range(n - 1):
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+            # Traverse from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+# Driver code to test above
+arr = [64, 34, 25, 12, 2, 1, 90]
+bubble_sort(arr)
+
+print("Sorted array is:")
+for element in arr:
+    print(element)
